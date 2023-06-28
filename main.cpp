020b6854a4e0b90d1c46f6c5700b8c320ec70375
Aleksandar Stefanovic <a.stefanovic1999@gmail.com>
2023-06-28 21:04:41 +0200

    Implemented the basic construction method for FiniteAutomaton

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include "finite_automaton.hpp"
 
+#include <iostream>
+
 int main(int argc, char *argv[])
 {
+    auto aut = FiniteAutomaton::construct(
+        {'a', 'b'},
+        {0, 1, 2},
+        {0},
+        {2},
+        {
+            {{0, 'a'}, {0, 1}},
+            {{0, 'b'}, {0}},
+            {{1, 'b'}, {2}}
+        }
+    );
+    
+    if (aut)
+        std::cout << "Success!" << std::endl;
+    else
+        std::cout << aut.error() << std::endl;
+        
     return 0;
 }
