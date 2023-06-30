d648e2d01c0ea07b5108b1990a76c6abf3b79e55
Aleksandar Stefanovic <a.stefanovic1999@gmail.com>
2023-06-30 19:03:36 +0200

    Implemented RegexDriver::parse

--- finite_automaton/regex_driver.cpp
+++ finite_automaton/regex_driver.cpp
@@ -1,3 +1,11 @@
 #include "regex_driver.hpp"
 
-std::unique_ptr<RegexAST> RegexDriver::parse(const std::string &regex) { return nullptr; }
+std::unique_ptr<RegexAST> RegexDriver::parse(const std::string &regex)
+{
+    string_scan_init(regex);
+    yy::parser parser(*this);
+    int res = parser();
+    string_scan_deinit();
+
+    return res != 0 ? std::move(m_ast) : nullptr;
+}
