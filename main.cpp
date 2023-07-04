ce09df70c83d76b805f4ebf275047305cf36cf50
Aleksandar Stefanovic <a.stefanovic1999@gmail.com>
2023-07-04 14:49:11 +0200

    Finished initial Qt integration

--- main.cpp
+++ main.cpp
@@ -1,26 +1,12 @@
 #include "finite_automaton.hpp"
+#include "main_window.hpp"
 
-#include <iostream>
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    auto aut = FiniteAutomaton::construct(
-        {'a', 'b'}, {0, 1, 2}, {0}, {2}, {{{0, 'a'}, {0, 1}}, {{0, 'b'}, {0}}, {{1, 'b'}, {2}}});
-
-    if (aut) {
-        std::cout << "aut_1" << std::endl;
-        std::cout << aut.value().accepts("abababab") << std::endl;
-        std::cout << aut.value().accepts("ababababb") << std::endl;
-        std::cout << aut.value().accepts("abaqabab") << std::endl;
-    } else
-        std::cout << aut.error() << std::endl;
-
-    auto aut_2 = FiniteAutomaton::construct("(ab|b*a+)*");
-    if (aut_2) {
-        std::cout << "aut_2" << std::endl;
-        std::cout << aut_2.value().accepts("ababbbbbba") << std::endl;
-    } else
-        std::cout << aut_2.error() << std::endl;
-
-    return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
---
+++ main_window.cpp
@@ -0,0 +1,6 @@
+#include "main_window.hpp"
+#include "./ui_main_window.h"
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) { ui->setupUi(this); }
+
+MainWindow::~MainWindow() { delete ui; }
