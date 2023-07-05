b70f3b5c7b7b8188a48898bab51107cb3ed5c0fc
Aleksandar Stefanovic <a.stefanovic1999@gmail.com>
2023-07-05 18:58:41 +0200

    Set up base for qt_graph

--- main_window.cpp
+++ main_window.cpp
@@ -1,6 +1,8 @@
 #include "main_window.hpp"
 #include "./ui_main_window.h"
 
+#include "graph.hpp"
+
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) { ui->setupUi(this); }
 
 MainWindow::~MainWindow() { delete ui; }
---
+++ qt_graph/graph.hpp
@@ -0,0 +1,10 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <QGraphicsItem>
+#include <graphviz/gvc.h>
+
+class Graph : public QGraphicsItem
+{};
+
+#endif // GRAPH_HPP
