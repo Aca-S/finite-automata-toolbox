9dd8a9e08b422056638cd2fd9c87b118bcf2fe4e
Aleksandar Stefanovic <a.stefanovic1999@gmail.com>
2023-09-19 12:34:20 +0200

    Implemented base AutomataScene class

--- ui/scene_tab_bar.cpp
+++ ui/scene_tab_bar.cpp
@@ -8,3 +8,9 @@ SceneTabBar::SceneTabBar(QGraphicsView *main_view, QWidget *parent) : QTabBar(pa
     this->setShape(QTabBar::RoundedNorth);
     this->addTab("Untitled");
 }
+
+void AutomataScene::set_scene_name(const QString &scene_name) { m_scene_name = scene_name; }
+
+QString AutomataScene::get_scene_name() { return m_scene_name; }
+
+bool AutomataScene::has_unsaved_changes() { return m_has_unsaved_changes; }
--- ui/scene_tab_bar.hpp
+++ ui/scene_tab_bar.hpp
@@ -13,4 +13,19 @@ class SceneTabBar : public QTabBar
     QGraphicsView *m_main_view;
 };
 
+class AutomataScene : public QGraphicsScene
+{
+  public:
+    AutomataScene(QWidget *parent = nullptr);
+
+    void set_scene_name(const QString &scene_name);
+    QString get_scene_name();
+
+    bool has_unsaved_changes();
+
+  private:
+    QString m_scene_name;
+    bool m_has_unsaved_changes = false;
+};
+
 #endif // UI_SCENE_TAB_BAR_HPP
